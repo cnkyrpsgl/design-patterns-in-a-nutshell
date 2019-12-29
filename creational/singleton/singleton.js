3b28d3a68134c3d3bb90769545eb7db4d9ce9fd4
Cenkay <cenkay.arapsaoglu@gmail.com>
2019-12-29 23:39:13 +0300

    added singleton

---
+++ creational/singleton/singleton.js
@@ -0,0 +1,15 @@
+class Singleton {
+	constructor() {
+		if (!Singleton.instance) {
+			Singleton.instance = this;
+		}
+		return Singleton.instance;
+	}
+
+	//rest is the same code as preceding example
+}
+
+const instance = new Singleton();
+Object.freeze(instance);
+
+export default instance;
